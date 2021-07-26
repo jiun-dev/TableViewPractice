24ca299405f517c55d201246ee5fafe196752c61
jiun-dev <jiun.dev@gmail.com>
2021-07-27 05:54:18 +0900

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by shinjiun on 2021/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
